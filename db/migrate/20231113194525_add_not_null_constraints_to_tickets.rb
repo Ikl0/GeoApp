39b485cd7d935976510c379cc35308567b7bef3d
ikl0 <klimovs2008van@gmail.com>
2023-11-13 23:26:50 +0300

    add constraints for ticket

---
+++ db/migrate/20231113194525_add_not_null_constraints_to_tickets.rb
@@ -0,0 +1,8 @@
+class AddNotNullConstraintsToTickets < ActiveRecord::Migration[7.0]
+  def change
+    change_column :tickets, :request_number, :string, null: false
+    change_column :tickets, :sequence_number, :bigint, null: false
+    change_column :tickets, :request_type, :string, null: false
+    change_column :tickets, :request_action, :string, null: false
+  end
+end
